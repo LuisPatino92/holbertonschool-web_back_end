42e18a98db981fe96a48d33f3ed9339c52ca66b0
Luis Patino <luapatinoar@unal.edu.co>
2022-09-23 18:47:53 -0500

    MRU

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Docstring Placeholder
+    """
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Adds """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS
+           and key not in self.cache_data.keys():
+
+            to_remove = list(self.cache_data.keys())[
+                BaseCaching.MAX_ITEMS - 1]
+            del self.cache_data[to_remove]
+            print("DISCARD: {}".format(to_remove))
+
+        if key in self.cache_data.keys():
+
+            del self.cache_data[key]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Gets """
+        
+        if key is None or key not in self.cache_data.keys():
+            return
+
+        item = self.cache_data[key]
+        self.put(key, item)
+        
+        return self.cache_data.get(key)
