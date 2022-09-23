2711a3d2e7798b4c2ec3c4e34896fed4e39432e5
Luis Patino <luapatinoar@unal.edu.co>
2022-09-23 18:37:36 -0500

    Basic cache implementation

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Docstring placeholder
+    """
+
+    def put(self, key, item):
+        """ Adds to cache"""
+
+        if item is None or key is None:
+            return
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Ggets from cache """
+
+        if key is None or key not in self.cache_data.keys():
+            return
+        else:
+            return self.cache_data.get(key)
