600752ab159513e70ac15140be71ed20db311e2f
Luis Patino <luapatinoar@unal.edu.co>
2022-09-23 18:54:59 -0500

    Docstring and style

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module """
+"""Module that has a more wide description """
 
 from base_caching import BaseCaching
 
@@ -7,19 +7,18 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """
     Docstring placeholder
+    This is a pretty functuion
     """
 
     def put(self, key, item):
-        """ Adds to cache"""
-
+        """ Adds to cache in a basic way"""
         if item is None or key is None:
             return
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Ggets from cache """
-
+        """ Ggets from cache  in basic way"""
         if key is None or key not in self.cache_data.keys():
             return
         else:
