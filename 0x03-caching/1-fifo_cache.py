1d6eae8b097b7b2a24aad1c836072454bcd1678d
Luis Patino <luapatinoar@unal.edu.co>
2022-09-28 17:31:37 -0500

    pep8 fix

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -17,7 +17,7 @@ class FIFOCache(BaseCaching):
         if key is None or item is None:
             return
         if (len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS
-            and key not in self.cache_data.keys()):
+                and key not in self.cache_data.keys()):
             to_remove = list(self.cache_data.keys())[0]
             del self.cache_data[to_remove]
             print("DISCARD: {}".format(to_remove))
