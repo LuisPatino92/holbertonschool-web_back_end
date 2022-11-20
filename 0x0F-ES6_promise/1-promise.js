84a3b1805bcc7ba4bd4bf0304b16a7e6e5e84872
Luis Patiño <2450@holbertonstudents.com>
2022-11-20 11:55:32 -0500

    returning from a variable

---
+++ 0x0F-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+function getFullResponseFromAPI(success) {
+  let prom = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return prom;
+}
+export default getFullResponseFromAPI;
