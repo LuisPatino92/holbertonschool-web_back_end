39394e500dda32f10e216843ac6c4776104c5eda
Luis Patiño <2450@holbertonstudents.com>
2022-11-22 22:46:49 -0500

    Style fix

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,10 @@
 function getListStudentIds(students) {
-  if (Array.isArray(students)){
+  if (Array.isArray(students)) {
     const ids = students.map((item) => item.id);
     return ids;
   }
+  
   return [];
 }
+
 export default getListStudentIds;
